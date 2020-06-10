cb8f7f2c7eb58ed9a59c017d030c3a3ddfcbccbd
IvanCantillo <49213723+IvanCantillo@users.noreply.github.com>
2020-06-09 22:19:19 -0500

    Update CasoModel.php

--- models/CasoModel.php
+++ models/CasoModel.php
@@ -53,7 +53,7 @@
         }
 
         public function getCasosById() {
-            $sqlCasos = 'SELECT * FROM casos WHERE fk_usuario = :fk_usuario AND id = :id';
+            $sqlCasos = 'SELECT * FROM casos WHERE id = :id';
             $casos = $this->conexion->prepare( $sqlCasos );
             $casos->execute( array( ":fk_usuario" => $this->fk_usuario, ":id" => $this->id ) );
             return $casos->fetch( PDO::FETCH_ASSOC );
